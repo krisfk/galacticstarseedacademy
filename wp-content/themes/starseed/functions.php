1a81d47cd9d418d8ab59ba45ff935e3bea808288
kaiyu li <krisfk@gmail.com>
2022-08-25 17:23:59 +0800

    .

--- wp-content/themes/starseed/functions.php
+++ wp-content/themes/starseed/functions.php
@@ -722,7 +722,7 @@ function my_front_end_login_fail( $username ) {
    $referrer = $_SERVER['HTTP_REFERER'];  // where did the post submission come from?
    // if there's a valid referrer, and it's not the default log-in screen
    if ( !empty($referrer) && !strstr($referrer,'wp-login') && !strstr($referrer,'wp-admin') ) {
-      wp_redirect( $referrer . '?login=failed' );  // let's append some information (login=failed) to the URL for the theme to use
+      wp_redirect( $referrer . '?f=1' );  // let's append some information (login=failed) to the URL for the theme to use
       exit;
    }
 }
