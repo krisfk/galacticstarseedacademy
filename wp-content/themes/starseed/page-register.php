c2fd93e1f2e04e97c72da9574a65002128ffcee7
kaiyu li <krisfk@gmail.com>
2022-09-06 01:05:14 +0800

    .

--- wp-content/themes/starseed/page-register.php
+++ wp-content/themes/starseed/page-register.php
@@ -12,7 +12,7 @@
 get_header();
 ?>
 
-test
+<!-- test -->
 <div class="container position-relative pt-5">
     <img class="star7 fadein-ele" style="  width: 200px !important;position: absolute;top: 49px;left: 485px;z-index: 0;"
         src="<?php echo get_template_directory_uri();?>/assets/images/star7.png" alt="">
