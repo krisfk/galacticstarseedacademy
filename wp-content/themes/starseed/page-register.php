73dccfb6867c276314127fac5c7006bbc4d79e69
kaiyu li <krisfk@gmail.com>
2022-09-06 01:21:04 +0800

    .

--- wp-content/themes/starseed/page-register.php
+++ wp-content/themes/starseed/page-register.php
@@ -27,16 +27,18 @@ get_header();
         src="<?php echo get_template_directory_uri();?>/assets/images/star9.png" alt="">
 
     <?php
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
 
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-endwhile; // End of the loop.
+    echo do_shortcode('[pms-register]');
+/* Start the Loop */
+// while ( have_posts() ) :
+// 	the_post();
+// 	get_template_part( 'template-parts/content/content-page' );
+
+// 	// If comments are open or there is at least one comment, load up the comment template.
+// 	if ( comments_open() || get_comments_number() ) {
+// 		comments_template();
+// 	}
+// endwhile; // End of the loop.
 ?>
 </div>
 <?php
