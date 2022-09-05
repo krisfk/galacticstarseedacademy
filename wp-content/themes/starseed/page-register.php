e40a427d570b0647a4fdbbe92ba98be0e5628a4c
kaiyu li <krisfk@gmail.com>
2022-09-06 01:37:18 +0800

    .

--- wp-content/themes/starseed/page-register.php
+++ wp-content/themes/starseed/page-register.php
@@ -48,7 +48,7 @@ $(function() {
     $('#pms_billing_city,#pms_billing_state').val('Hong Kong');
     $('#pms_billing_country').val('HK');
 
-    JQuery('.pms-form-submit').click(function() {
+    jQuery('.pms-form-submit').click(function() {
         $(this).value('ddd')
     })
 
