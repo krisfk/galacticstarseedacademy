fe7b4169f0194fcf86cd77d99839952fd1ebab68
kaiyu li <krisfk@gmail.com>
2022-09-30 16:35:40 +0800

    .

--- wp-content/themes/starseed/header.php
+++ wp-content/themes/starseed/header.php
@@ -36,7 +36,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <script type="text/javascript" src="<?php echo get_template_directory_uri();?>/assets/js/common.js"></script>
+    <!-- <script type="text/javascript" src="<?php echo get_template_directory_uri();?>/assets/js/common.js"></script> -->
     <script type="text/javascript" src="<?php echo get_template_directory_uri();?>/player.css"></script>
 
 </head>
