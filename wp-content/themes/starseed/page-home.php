0b06370af2e4b2ae24e0aba564e570fcd5ba104e
kaiyu li <krisfk@gmail.com>
2023-03-14 00:09:04 +0900

    .

--- wp-content/themes/starseed/page-home.php
+++ wp-content/themes/starseed/page-home.php
@@ -109,7 +109,8 @@ get_header(); ?>
         </div>
         <div class="col-lg-4 col-md-12 col-sm-12 col-12 ">
             <img class="w-100 bosco-foto"
-                src="<?php echo get_template_directory_uri();?>/assets/images/Pro-Pic-1-600x600.jpeg" alt="">
+                src="https://galacticstarseedacademy.com/wp-content/themes/starseed/assets/images/Pro-Pic-1-600x600.jpeg"
+                alt="">
 
         </div>
 
